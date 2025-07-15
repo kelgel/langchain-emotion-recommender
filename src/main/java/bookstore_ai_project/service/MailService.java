69a678b2fc31cd098a5f341175241ccbd9731c63
seonghoon <seonghoon315@naver.com>
2025-07-15 10:13:09 +0900

    refactor: MailService_발신자 하드코딩 제거

--- src/main/java/bookstore_ai_project/service/MailService.java
+++ src/main/java/bookstore_ai_project/service/MailService.java
@@ -2,6 +2,7 @@ package bookstore_ai_project.service;
 
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,19 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${mail.sender.email}")
+    private String senderEmail;
+
+    @Value("${mail.sender.name}")
+    private String senderName;
+
     public void sendTempPasswordMail(String to, String tempPassword) throws Exception {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
 
         helper.setTo(to);
         helper.setSubject("[YESorNO.24] 임시 비밀번호 안내");
-        helper.setFrom("seonghoon95@gmail.com", "YESorNO.24 관리자");
+        helper.setFrom(senderEmail, senderName);
         helper.setText(
                 "안녕하세요. YESorNO.24입니다.\n\n" +
                 "임시 비밀번호: " + tempPassword + "\n\n" +
